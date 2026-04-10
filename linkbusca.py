84d08d1b7970299ef41a47afc91a25c703e63b84
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2026-04-10 14:34:05 -0300

    teste

--- linkbusca.py
+++ linkbusca.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import os
 
 
@@ -15,12 +15,12 @@ def obter_link_busca():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--disable-software-rasterizer")
 
-    if os.environ.get("RENDER"):
-        options.binary_location = "/usr/bin/chromium"
+    options.binary_location = "/usr/bin/chromium"
 
-    driver = webdriver.Chrome(options=options)
+    service = Service("/usr/bin/chromedriver")
+
+    driver = webdriver.Chrome(service=service, options=options)
     try:
         driver.get("https://www.in.gov.br/materia")
 
